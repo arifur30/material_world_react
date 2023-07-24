f19e1c41b0184f0003f480e0a97c96c7e9715f78
Jifat <jifat@Buggy.buggy.com>
2023-07-24 17:12:29 +0600

    made the initial works

--- src/index.js
+++ src/index.js
@@ -1,34 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
-import { storeManager } from "./store/index";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-import CommonMain from "./screens/common/CommonMain";
-import Welcome from "./screens/common/Welcome";
-import PageOne from "./components/common/PageOne";
-import PageTwo from "./components/common/PageTwo";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<Provider store={storeManager.configureStore()}>
-		<PersistGate loading={false} persistor={storeManager.persistor}>
-			<BrowserRouter>
-				<Routes>
-					{/* I use a 'door' Route to access all the related routes (for Auth and/or other shared purposes) */}
-					<Route path="/" element={<CommonMain />}>
-						<Route path="/" element={<Welcome />} />
-						<Route path="/pageone" element={<PageOne />} />
-						<Route path="/pagetwo" element={<PageTwo />} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
-		</PersistGate>
-	</Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
-reportWebVitals();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
--- src/store/types/commonTypes.js
+++
@@ -1,7 +0,0 @@
-export const commonTypes = {
-	AsyncStarted: "COMMON.ASYNC_STARTED",
-	AsyncEnd: "COMMON.ASYNC_END",
-	AsyncError: "COMMON.ASYNC_ERROR",
-	ClearError: "COMMON.CLEARERROR",
-	GlobalError: "COMMON.GLOBALERROR",
-};
