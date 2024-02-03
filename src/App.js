c13dded3011297b5ef912003c4090fe26eb0a66b
FarhanaIslamSaima <farhanachowdhury2152001@gmail.com>
2024-02-03 18:12:21 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import { Route, BrowserRouter, Routes ,} from "react-router-dom";
-import Banner from "./pages/Banner";
-import Offers from "./pages/Offers";
-import Profile from "./pages/Profile";
 
-import LatestNews from "./component/LatestNews";
 
 import Footer from "./component/Footer";
 import Header from "./component/Header";
-import TopStories from "./component/TopStories";
+
 import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import News from "./pages/News/News";
@@ -15,7 +11,7 @@ import NewsDetails from "./pages/News/NewsDetails";
 import Developers from "./pages/Developers"
 import { useEffect } from "react";
 import ReactGA from 'react-ga'
-import { usePageTracking } from "./component/Tracking";
+
 import { useLocation } from "react-router-dom";
 
 
@@ -30,7 +26,7 @@ function App() {
   }, [location]);
   return (
     <>
-      <BrowserRouter >
+      
         <Header />
         <Routes>
          
@@ -41,7 +37,7 @@ function App() {
           <Route path="/dev" element={<Developers/>}></Route>
         </Routes>
         <Footer />
-      </BrowserRouter>
+      
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Loader from './component/Loader';
-
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     {window.onload?
-    <Loader /> : <App />}
+    <Loader /> : <BrowserRouter><App /></BrowserRouter>}
   </React.StrictMode>
 );
 
